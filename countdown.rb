01b78eac87ce2f11b30b3ac37b5f512e970f2e5e
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-27 17:16:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,8 +11,8 @@ end
 def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECONDS(S)!"
-    number -=1
     sleep(1.minutes)
+    number -=1
   end
   puts "HAPPY NEW YEAR!"
 end
